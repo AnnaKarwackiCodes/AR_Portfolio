454e1c50ff5b09ea51a7fa9260ceb7c79492f99e
Anna Karwacki <avk9238@rit.edu>
2018-08-15 17:36:23 -0400

    Scene Swtiching

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,36 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public GameObject UI;
-    private Vector3 closedPos;
-    private Vector3 openPos;
-    public GameObject Model;
 
     private bool MenuOpen;
 
 	// Use this for initialization
 	void Start () {
-        MenuOpen = false;
-        closedPos = new Vector3(0, 0, 0);
-        openPos = new Vector3(-300, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
-    public void CameraMenu() {
-        if (!MenuOpen)
-        {
-            UI.GetComponent<Animator>().SetBool("IsDisplayed", true);
-            MenuOpen = true;
-        }
-        else
-        {
-            UI.GetComponent<Animator>().SetBool("IsDisplayed", false);
-            MenuOpen = false;
-        }
+    public void MenuSwitch(string screen)
+    {
+        SceneManager.LoadScene(screen);
     }
 }
